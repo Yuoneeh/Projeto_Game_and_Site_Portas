6f4de350e34ed489d870012939c884d83df06342
Eduardo Braga <Dudufelipeeduard@gmail.com>
2025-05-20 10:19:33 -0300

    AAAA

--- Godot/Cadeia_logica_Gdscript/scenes/music_manager/music_player.gd
+++ Godot/Cadeia_logica_Gdscript/scenes/music_manager/music_player.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var musica_1: AudioStreamPlayer = $musica_1
+@onready var musica_2: AudioStreamPlayer = $musica_2
+@onready var musica_3: AudioStreamPlayer = $musica_3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func play_song_1():
+	musica_1.play()
+
+
+func play_song_2():
+	musica_2.play()
